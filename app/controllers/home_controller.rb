848df1e7b2ce9cd6e4d1533485a322f631a551dd
kyoh <stayheresilence@gmail.com>
2017-04-21 15:34:23 +0900

    create lib

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+  class Form
+    include ActiveModel::Model
+
+    attr_accessor :attractions
+  end
   def index
+    @attractions = Attraction.all
+    @form = Form.new(attractions: @attractions)
   end
 
   def calc
