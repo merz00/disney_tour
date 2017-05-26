32313448a6faa52018dfe92f0e8edf74dbc71592
Ricky <masataka-goda@g.ecc.u-tokyo.ac.jp>
2017-05-26 13:50:00 +0900

    テストファイルの追加

---
+++ lib/others/cpp/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <sstream>
+#include <cstdio>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <map>
+#include <stdlib.h>
+#include <unistd.h>
+#include "picojson.h"
+
+using namespace std;
+using namespace std::chrono;
+
+int main()
+{
+    static char buf[1024] = {};
+    readlink("/proc/self/exe",buf,sizeof(buf)-1);
+    current_directory = string(buf)
+    cout << current_directory << "\n";
+}
